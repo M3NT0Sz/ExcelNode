8799c23bb3be672704136264a40eef5585bfe121
M3NT0Sz <matheusmendes2005@outlook.com>
2024-05-25 19:28:27 -0300

    Adicionando Arquivos

--- app.js
+++ app.js
@@ -1,48 +1,37 @@
-const ExcelJS = require("exceljs");
+const XLSX = require("xlsx");
 const fs = require("fs");
 
-// Carregar o arquivo Excel
-const workbook = new ExcelJS.Workbook();
-workbook.xlsx
-  .readFile("./ModeloCreatorAuthor.xlsx")
-  .then(() => {
-    // Assume que o arquivo tem apenas uma planilha, caso contrário, você precisará iterar sobre as planilhas
-    const worksheet = workbook.getWorksheet(1);
+// Ler o arquivo Excel
+const workbook = XLSX.readFile("./ModeloCreatorAuthor.xlsx");
 
-    // Converter os dados da planilha para JSON
-    const jsonData = [];
-    worksheet.eachRow({ includeEmpty: true }, (row, rowNumber) => {
-      if (rowNumber !== 1) {
-        const rowData = {};
-        row.eachCell({ includeEmpty: true }, (cell, colNumber) => {
-          rowData[`col${colNumber}`] = cell.value;
-        });
-        jsonData.push(rowData);
-      }
-    });
+// Obter a primeira planilha
+const sheetName = workbook.SheetNames[0];
+const worksheet = workbook.Sheets[sheetName];
 
-    // Filtrar as linhas com base no valor da coluna "tipo"
-    const filteredJson = jsonData.filter((row) => {
-      return row.col1 !== null; // Manter a linha se o valor da coluna "tipo" não for null
-    });
+// Converter a planilha para JSON
+const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
 
-    // Transformar o JSON filtrado em um formato ajustado
-    const adjustedJson = filteredJson.map((row) => ({
-      tipo: row.col1,
-      enunciado: row.col2,
-      imagemEnunciado: row.col3,
-      resposta: row.col4,
-      opcaoA: row.col5,
-      opcaoB: row.col6,
-      opcaoC: row.col7,
-      opcaoD: row.col8,
-      // Adicione mais campos conforme necessário
-    }));
+// Obter cabeçalhos e linhas
+const headers = jsonData[0];
+const rows = jsonData.slice(1);
 
-    // Salvar os dados em um arquivo JSON
-    fs.writeFileSync("dados.json", JSON.stringify(adjustedJson, null, 4));
-    console.log("Arquivo JSON gerado com sucesso!");
-  })
-  .catch((error) => {
-    console.error("Erro ao ler o arquivo Excel:", error);
-  });
+// Filtrar linhas onde o primeiro valor (tipo) não é nulo
+const filteredJson = rows.filter((row) => row[0] !== null);
+
+// Mapear as linhas para o formato desejado
+const adjustedJson = filteredJson.map((row) => ({
+  tipo: row[0],
+  enunciado: row[1],
+  resposta: row[3],
+  opcaoA: row[4],
+  opcaoB: row[5],
+  opcaoC: row[6],
+  opcaoD: row[7],
+}));
+
+// Remover objetos vazios ou com todas as propriedades indefinidas
+const finalJson = adjustedJson.filter(obj => Object.values(obj).some(val => val !== undefined && val !== null));
+
+// Salvar os dados em um arquivo JSON
+fs.writeFileSync("dados.json", JSON.stringify(finalJson, null, 4));
+console.log("Arquivo JSON gerado com sucesso!");
