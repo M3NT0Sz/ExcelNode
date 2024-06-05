6079fbe9dda8f96a4f20d914a48d7727e52d2e8a
M3NT0Sz <matheusmendes2005@outlook.com>
2024-06-05 16:24:42 -0300

    Aprimorando o plugin SheetJS usando o ExcelJS AB#34

--- sheetjs.js
+++ sheetjs.js
@@ -4,26 +4,25 @@ const ExcelJS = require("exceljs");
 
 // Função para remover propriedades com valores null ou undefined de um objeto
 function removeNullProperties(obj) {
-  return Object.fromEntries(
-    Object.entries(obj).filter(([_, v]) => v != null)
-  );
+  return Object.fromEntries(Object.entries(obj).filter(([_, v]) => v != null));
 }
 
 // Ler o arquivo Excel com ExcelJS
 const workbook = new ExcelJS.Workbook();
-workbook.xlsx.readFile("./ModeloCreatorAuthor.xlsx")
+workbook.xlsx
+  .readFile("./ModeloCreatorAuthor.xlsx")
   .then(() => {
     const worksheet = workbook.getWorksheet(1); // Obter a primeira planilha
 
     const jsonData = {
-      "licao": []
+      licao: [],
     };
 
     // Iterar sobre as linhas da planilha, começando da segunda linha para evitar a primeira linha (cabeçalho)
     for (let i = 2; i <= worksheet.rowCount; i++) {
       let rowData = {};
       let tipo = worksheet.getRow(i).getCell(1).value;
-      
+
       if (tipo === "Multipla Escolha") {
         rowData = {
           tipo: tipo,
@@ -33,17 +32,65 @@ workbook.xlsx.readFile("./ModeloCreatorAuthor.xlsx")
             opcaoA: worksheet.getRow(i).getCell(4).value,
             opcaoB: worksheet.getRow(i).getCell(5).value,
             opcaoC: worksheet.getRow(i).getCell(6).value,
-            opcaoD: worksheet.getRow(i).getCell(7).value
-          }
+            opcaoD: worksheet.getRow(i).getCell(7).value,
+          },
         };
         rowData.opcoes = removeNullProperties(rowData.opcoes);
       } else if (tipo === "Preenchimento") {
         rowData = {
           tipo: tipo,
           enunciado: worksheet.getRow(i).getCell(2).value,
-          resposta: worksheet.getRow(i).getCell(3).value
+          resposta: worksheet.getRow(i).getCell(3).value,
         };
-      } else if (tipo )
+      } else if (tipo === "Organizar") {
+        rowData = {
+          tipo: tipo,
+          enunciado: worksheet.getRow(i).getCell(2).value,
+          resposta: worksheet.getRow(i).getCell(3).value,
+          opcoes: {
+            opcaoA: worksheet.getRow(i).getCell(4).value,
+            opcaoB: worksheet.getRow(i).getCell(5).value,
+            opcaoC: worksheet.getRow(i).getCell(6).value,
+            opcaoD: worksheet.getRow(i).getCell(7).value,
+          },
+        };
+      } else if (tipo === "Arrasta e Solta") {
+        rowData = {
+          tipo: tipo,
+          enunciado: worksheet.getRow(i).getCell(2).value,
+          resposta: worksheet.getRow(i).getCell(3).value,
+          opcoes: {
+            opcaoA: worksheet.getRow(i).getCell(4).value,
+            opcaoB: worksheet.getRow(i).getCell(5).value,
+            opcaoC: worksheet.getRow(i).getCell(6).value,
+            opcaoD: worksheet.getRow(i).getCell(7).value,
+          },
+        };
+      } else if (tipo === "Associar") {
+        rowData = {
+          tipo: tipo,
+          enunciado: worksheet.getRow(i).getCell(2).value,
+          resposta: worksheet.getRow(i).getCell(3).value,
+          opcoes: {
+            opcaoA: worksheet.getRow(i).getCell(4).value,
+            opcaoB: worksheet.getRow(i).getCell(5).value,
+            opcaoC: worksheet.getRow(i).getCell(6).value,
+            opcaoD: worksheet.getRow(i).getCell(7).value,
+          },
+        };
+      } else if (tipo === "Jogo da memória") {
+        rowData = {
+          tipo: tipo,
+          enunciado: worksheet.getRow(i).getCell(2).value,
+          resposta: worksheet.getRow(i).getCell(3).value,
+          opcoes: {
+            opcaoA: worksheet.getRow(i).getCell(4).value,
+            opcaoB: worksheet.getRow(i).getCell(5).value,
+            opcaoC: worksheet.getRow(i).getCell(6).value,
+            opcaoD: worksheet.getRow(i).getCell(7).value,
+          },
+        };
+      }
 
       rowData = removeNullProperties(rowData);
 
@@ -54,9 +101,9 @@ workbook.xlsx.readFile("./ModeloCreatorAuthor.xlsx")
     }
 
     // Escrever o JSON em um arquivo
-    fs.writeFileSync('dadosSheet.json', JSON.stringify(jsonData, null, 2));
-    console.log('Arquivo JSON salvo em dadosSheet.json');
+    fs.writeFileSync("dadosSheet.json", JSON.stringify(jsonData, null, 2));
+    console.log("Arquivo JSON salvo em dadosSheet.json");
   })
-  .catch(err => {
-    console.error('Erro ao processar o arquivo:', err);
+  .catch((err) => {
+    console.error("Erro ao processar o arquivo:", err);
   });
