f747c7d8e3e1757bd063c28718997f0ab7d9d9b0
M3NT0Sz <matheusmendes2005@outlook.com>
2024-09-25 16:42:37 -0300

    Algo

--- exceljs/exceljs.js
+++ exceljs/exceljs.js
@@ -1,7 +1,6 @@
-const ExcelJS = require('exceljs');
-const fs = require('fs');
-const axios = require('axios');
-const path = require('path');
+const ExcelJS = require("exceljs");
+const fs = require("fs");
+const axios = require("axios");
 
 async function excelToJson(filePath, sheetName) {
     const workbook = new ExcelJS.Workbook();
@@ -9,28 +8,84 @@ async function excelToJson(filePath, sheetName) {
     const worksheet = workbook.getWorksheet(sheetName);
 
     const data = [];
-    const imageMap = {};
 
     const processRow = async (row, rowNumber) => {
         if (rowNumber > 1) {
             const rowData = {
-                Tipo: row.getCell(1).value,
-                Enunciado: row.getCell(2).value,
-                Resposta: row.getCell(3).value,
-                OpcaoA: row.getCell(4).value,
-                OpcaoB: row.getCell(5).value,
-                OpcaoC: row.getCell(6).value,
-                OpcaoD: row.getCell(7).value,
-                Imagens: {}
+                id: rowNumber.toString(),
+                tipo: row.getCell(1).value || "",
+                contexto: [
+                    {
+                        tipo: "texto",
+                        dados: {
+                            html: row.getCell(2).value || "",
+                            audio: "",
+                            centralizado: "false",
+                        },
+                    },
+                ],
+                alternativas: [
+                    {
+                        id: "A",
+                        img: "",
+                        descricao: row.getCell(4).value || "",
+                        distrator: "",
+                        "exibir-descricao": "false",
+                        resposta:
+                            row.getCell(3).value === row.getCell(4).value
+                                ? "correta"
+                                : "incorreta",
+                    },
+                    {
+                        id: "B",
+                        img: "",
+                        descricao: row.getCell(5).value || "",
+                        distrator: "",
+                        "exibir-descricao": "false",
+                        resposta:
+                            row.getCell(3).value === row.getCell(5).value
+                                ? "correta"
+                                : "incorreta",
+                    },
+                    {
+                        id: "C",
+                        img: "",
+                        descricao: row.getCell(6).value || "",
+                        distrator: "",
+                        "exibir-descricao": "false",
+                        resposta:
+                            row.getCell(3).value === row.getCell(6).value
+                                ? "correta"
+                                : "incorreta",
+                    },
+                    {
+                        id: "D",
+                        img: "",
+                        descricao: row.getCell(7).value || "",
+                        distrator: "",
+                        "exibir-descricao": "false",
+                        resposta:
+                            row.getCell(3).value === row.getCell(7).value
+                                ? "correta"
+                                : "incorreta",
+                    },
+                ],
+                feedback: {
+                    tipo: "opcao3",
+                    dados: {},
+                },
+                "is-professor": "false",
             };
 
             const imageUrl = row.getCell(8).value;
             if (imageUrl) {
                 const imageName = `image_${rowNumber}.png`;
                 try {
-                    const imageResponse = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+                    const imageResponse = await axios.get(imageUrl, {
+                        responseType: "arraybuffer",
+                    });
                     fs.writeFileSync(imageName, imageResponse.data);
-                    rowData.Imagens['URL'] = path.resolve(imageName);
+                    rowData.contexto[0].dados.img = imageName;
                 } catch (error) {
                     console.error(`Erro ao baixar a imagem ${imageUrl}:`, error.message);
                 }
@@ -39,16 +94,29 @@ async function excelToJson(filePath, sheetName) {
             const images = worksheet.getImages();
             for (const image of images) {
                 if (image.range.tl.nativeRow === rowNumber - 1) {
-                    const img = workbook.model.media.find(m => m.index === image.imageId);
+                    const img = workbook.model.media.find(
+                        (m) => m.index === image.imageId
+                    );
                     if (img) {
-                        const imageFileName = `embedded_${rowNumber}_${image.range.tl.nativeCol}.${img.name}.${img.extension}`;
-                        fs.writeFileSync(imageFileName, img.buffer);
-                        rowData.Imagens[`Coluna${image.range.tl.nativeCol + 1}`] = path.resolve(imageFileName);
+                        const colIndex = image.range.tl.nativeCol - 3;
+                        if (colIndex >= 0 && colIndex < rowData.alternativas.length) {
+                            const imageFileName = `embedded_${rowNumber}_${image.range.tl.nativeCol}.${img.name}.${img.extension}`;
+                            fs.writeFileSync(imageFileName, img.buffer);
+                            rowData.alternativas[colIndex].img = imageFileName;
+                        }
                     }
                 }
             }
 
-            data.push(rowData);
+            // Check if rowData matches the specified structure
+            const isEmptyRowData =
+                rowData.tipo === "" &&
+                rowData.contexto[0].dados.html === "" &&
+                rowData.alternativas.every((alt) => alt.descricao === "");
+
+            if (!isEmptyRowData) {
+                data.push(rowData);
+            }
         }
     };
 
@@ -57,9 +125,30 @@ async function excelToJson(filePath, sheetName) {
         await processRow(row, i);
     }
 
-    const json = JSON.stringify(data, null, 2);
-    fs.writeFileSync('dataExceljs.json', json, 'utf8');
-    console.log('Arquivo JSON criado com sucesso!');
+    if (data.length > 0) {
+        const json = JSON.stringify(data, null, 2);
+        fs.writeFileSync("dataExceljs.json", json, "utf8");
+        console.log("Arquivo JSON criado com sucesso!");
+
+        data.forEach((rowData, index) => {
+            const json = JSON.stringify(rowData, null, 2);
+            fs.writeFileSync(`dataExceljs_${index + 1}.json`, json, "utf8");
+
+            // Verificar qual alternativa está correta
+            const correctAlternative = rowData.alternativas.find(
+                (alt) => alt.resposta === "correta"
+            );
+            if (correctAlternative) {
+                console.log(
+                    `A alternativa correta para a questão ${rowData.id} é: ${correctAlternative.id}`
+                );
+            } else {
+                console.log(`Nenhuma alternativa correta encontrada para a questão ${rowData.id}`);
+            }
+        });
+    } else {
+        console.log("Nenhum dado encontrado para criar arquivos JSON.");
+    }
 }
 
-excelToJson('../ModeloCreatorAuthor.xlsx', 'Planilha1').catch(console.error);
+excelToJson("../ModeloCreatorAuthor.xlsx", "Planilha1").catch(console.error);
